305a5b6f7c560605d700f8c7d4b2609c46be4fc7
Elias Simon <riesyeti@outlook.de>
2020-05-28 16:00:14 +0200

    initial commit

---
+++ BlockCat.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0,0)
+const SPEED = 150 # do this HERE, then u only need to change this once
+const GRAVITY = 10
+const JUMPFORCE = -220
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("right"):
+		velocity.x = SPEED
+		$Sprite.play("walkright") #$ accesses the node's children / names of the animations in AnimatedSprite
+		
+	elif Input.is_action_pressed("left"):
+		velocity.x = -SPEED
+		$Sprite.play("walkleft")
+		
+	else:
+		$Sprite.play("idle")
+		
+	
+	
+	velocity.y = velocity.y + GRAVITY # basically the gravity
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor(): # just_pressed makes it a "press once" thing
+		velocity.y = JUMPFORCE
+	
+	move_and_slide(velocity) # function of movement, variable is our velocity var with the Vector2.x
+	velocity = move_and_slide(velocity, Vector2.UP) # fixes the infinitely adding gravity from the velocity.y + X thing
+	
+	velocity.x = lerp(velocity.x,0,0.2) # lerp makes velocity closer to 0 over time with the velocity value
+	# the last value sets the speed of the slowdown
---
+++ Enemy1.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+const SPEED = 100
+const GRAVITY = 10
+const JUMPFORCE = -220
+const FLOOR = Vector2(0, -1)
+
+
+var direction = 1
+
+var is_dead = false
+
+func dead():
+	is_dead = true
+	velocity = Vector2(0,0) # stop enemy
+
+func _physics_process(delta):
+	velocity.x = SPEED * direction # calculates direction the enemy moves towards
+	velocity.y = velocity.y + GRAVITY
+	
+	# with animations
+	# if direction == 1:
+		#$AnimatedSprite.flip_h = false
+	#else:
+		#$AnimatedSprite.flip_h = true
+	#$AnimatedSprite.play("animationName")
+	
+	velocity = move_and_slide(velocity, FLOOR)
+	
+	if is_dead:
+		queue_free()
+	
+	if is_on_wall():
+		direction = direction * -1 # "flips" the direction, und - plus - ist ja plus, also geht es in beide Richtungen
+		# but the RayCast still needs to be flipped
+		$RayCast2D.position.x *= -1 # will put it on the other side of the enemy
+	# make sure the RayCast Arrow is underneath the enemie's collisionshape
+	if $RayCast2D.is_colliding() == false:
+		direction = direction * -1 # the same thing above, when it's "not colliding"
+		$RayCast2D.position.x *= -1
+		
+	if $RayCast_Top.is_colliding():
+		queue_free()
