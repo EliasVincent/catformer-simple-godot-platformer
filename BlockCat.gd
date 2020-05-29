e855f37eb501fc57ac1f0e1e504ef5016b1f807f
Elias Simon <riesyeti@outlook.de>
2020-05-29 17:22:53 +0200

    a respawn system which just reloads the scene, instanced deadly spike

--- BlockCat.gd
+++ BlockCat.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
+var doublejump = 0
 var velocity = Vector2(0,0)
+var isDed = false
+var spawnPoint = Vector2(144, 110.566002)
 const SPEED = 150 # do this HERE, then u only need to change this once
 const GRAVITY = 10
 const JUMPFORCE = -220
@@ -10,9 +13,13 @@ export var stomp_impulse: = 500
 #	velocity = calculate_stomp_velocity(velocity, stomp_impulse)
 	
 
-#func _on_Area2D_EnemyDetector_body_entered(body):
-	#queue_free()
-
+func _on_Area2D_EnemyDetector_body_entered(body):
+	if body.is_in_group("enemy"):
+		get_tree().reload_current_scene()
+func _ready():
+	var pos = get_position()
+	print(pos)
+	return pos
 
 func _physics_process(delta):
 	
@@ -31,15 +38,22 @@ func _physics_process(delta):
 	
 	velocity.y = velocity.y + GRAVITY # basically the gravity
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor(): # just_pressed makes it a "press once" thing
+	if Input.is_action_pressed("jump") and is_on_floor() and doublejump < 2: # just_pressed makes it a "press once" thing
 		velocity.y = JUMPFORCE
+	if Input.is_action_just_pressed("jump"):
+		doublejump = doublejump + 1
+		print(doublejump)
+	if is_on_floor():
+		doublejump = 0
 	
 	move_and_slide(velocity) # function of movement, variable is our velocity var with the Vector2.x
 	velocity = move_and_slide(velocity, Vector2.UP) # fixes the infinitely adding gravity from the velocity.y + X thing
 	
 	velocity.x = lerp(velocity.x,0,0.2) # lerp makes velocity closer to 0 over time with the velocity value
 	# the last value sets the speed of the slowdown
-
+	if position.y > 367:
+		print("H")
+		get_tree().reload_current_scene()
 #func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
 	#var out: = linear_velocity
 	#out.y = -impulse 
