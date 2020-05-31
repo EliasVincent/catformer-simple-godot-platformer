52c888913b4302566a9c16da7ad0118bc904832c
Elias Simon <riesyeti@outlook.de>
2020-05-31 16:15:34 +0200

    pre-acceleration

--- scripts/MenuPanel.gd
+++ scripts/MenuPanel.gd
@@ -7,3 +7,11 @@ func _ready():
 
 func _on_playbutton_pressed():
 	get_tree().change_scene("res://scenes/Level1.tscn")
+
+
+func _on_linkbutton_pressed():
+	OS.shell_open("http://godotengine.org")
+
+
+func _on_aboutbutton_pressed():
+	$"about-button/about-panel".show()
